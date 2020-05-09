5a34d8d82f87b27c8b31c4ad211615b6c535716f
Jose Jimenez <email@jimenezjose.com>
2020-05-09 16:27:25 -0500

    set up api service

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../services/api";
 
 function Home(props) {
+  const [entries, setEntries] = useState(null);
+
+  useEffect(() => {
+    api.getAllEntries().then((entries) => setEntries(entries.data));
+  }, []);
+
+  let recentEntries = entries
+    ? entries.map((entry) => <p key={entry.id}>{entry.body}</p>)
+    : "loading...";
+
   return (
     <div>
       <h1>Home</h1>
+      {recentEntries}
     </div>
   );
 }
---
+++ client/src/services/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `http://localhost:5000/socialapp-94844/us-central1/api`,
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  signup(userData) {
+    return api.post("/signup", userData);
+  },
+
+  signin(userData) {
+    return api.post("/signin", userData);
+  },
+
+  getAllEntries() {
+    return api.get("/entries");
+  },
+};
