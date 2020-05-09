630ef945bb08e85ade9e106f25d09929720f62e1
Jose Jimenez <email@jimenezjose.com>
2020-05-09 16:26:35 -0500

    add cors to server

--- functions/index.js
+++ functions/index.js
@@ -32,6 +32,16 @@ const {
   markNotificationsRead,
 } = require("./handlers/notifications");
 
+// CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Entries routes
 app.get("/entries", getAllEntries);
 app.get("/entry/:id", getEntry);
